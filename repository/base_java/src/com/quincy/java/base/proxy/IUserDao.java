cd6f2a277acafb83b13e51f48c130f3634d11c38
何其有幸 <git@git.com>
2018-12-16 11:05:41 +0800

    提交

---
+++ repository/base_java/src/com/quincy/java/base/proxy/IUserDao.java
@@ -0,0 +1,11 @@
+package com.quincy.java.base.proxy;
+
+/**
+ * Created by quincy on 18/6/16.
+ */
+public interface IUserDao {
+
+    public void save(String str);
+}
+
+
---
+++ repository/base_java/src/com/quincy/java/base/sets/MyArrayList.java
@@ -0,0 +1,17 @@
+package com.quincy.java.base.sets;
+
+import java.util.ArrayList;
+
+/**
+ * Created by quincy on 18/5/13.
+ */
+public class MyArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> lists = new ArrayList<>();
+        lists.add("qw");
+
+
+        lists.contains("String");
+
+    }
+}
